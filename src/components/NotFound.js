c03fbabd83af9f806e3389fd2b53ee52b0d4c0bf
danici <danici@email.com>
2023-11-08 10:46:59 +0100

    Dealing with errors

---
+++ src/components/NotFound.js
@@ -0,0 +1,3 @@
+export default function NotFound(){
+    return <p>Page you are lookig for not found</p>
+}
--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,27 +1,76 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import NotFound from "../components/NotFound";
 
 export default function Definition() {
     const [word, setWord] = useState()
+    const [notFound, setNotFound] = useState(false)
+    const [error, setError] = useState(false)
     //console.log(useParams())
     let { search } = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+search)
-            .then((response) => response.json())
+        //const url = 'https://httpstat.us/500'
+        const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'+search
+        fetch(url)
+            .then((response) => {
+                if(response.status === 404) {
+                    setNotFound(true)
+                } else if (response.status === 401) {
+                    navigate('/login')
+                } else if (response.status === 500) {
+                    setError(true)
+                }
+
+                if (!response.ok) {
+                    setError(true)
+                    throw new Error('Something went wrong')
+                }
+                return response.json()
+            }
+            )
             .then((data) => {
                 setWord(data[0].meanings)
                 // console.log(data[0].meanings)
-            });
+            })
+            .catch((e) => {
+                console.log(e.message)
+            })
     },[]);
+
+    if (notFound === true) {
+        return(
+        <>
+         <NotFound />
+         <Link to='/dictionary'>Search another</Link>
+         </>
+        )
+    }
+
+    if (error === true) {
+        return(
+        <>
+         <p>Something went wrong, try again ?</p>
+         <Link to='/dictionary'>Search another</Link>
+         </>
+        )
+    }
     
     return (
     <>
-     <h1>Here is a Definition Page</h1>
-     {word ? word.map((meaning) =>{
-        return <p key={uuidv4()}>{meaning.partOfSpeech}: {meaning.definitions[0].definition}</p>
-     }): <p>Loading...</p>}
+     {word ? (
+        <>
+        <h1>Here is a Definition Page:</h1>
+         {word.map((meaning) =>{
+        return (<p key={uuidv4()}>
+            {meaning.partOfSpeech}: {meaning.definitions[0].definition}
+            </p>
+        )
+     })}
+    </>
+    ): null}
     </>
     );
 }
