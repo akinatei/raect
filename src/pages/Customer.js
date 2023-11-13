2a4765790a3361dbefd062a11e5eb877bb7dffff
danici <danici@email.com>
2023-11-13 10:59:24 +0100

    deleting and adding from frontend

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,6 +1,6 @@
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import NotFound from "../components/NotFound";
+//import NotFound from "../components/NotFound";
 import { baseUrl } from "../shared";
 
 export default function Customer(){
@@ -24,7 +24,12 @@ export default function Customer(){
     })
         .then((data) => {setCustomer(data.customer)
         });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+    
+    // function deleteCustomer() {
+    //     console.log('deleting...')
+    // }
     return( 
         <>
         {notFound ? <p>The page of {id} is not in our database</p> : null}
@@ -33,6 +38,23 @@ export default function Customer(){
             <p>{customer.name}</p>
             <p>{customer.industry}</p>
         </div>: null}
+        <button onClick={(e) => {
+            const url = baseUrl+'api/customers/'+id
+            fetch(url, { method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }, })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong')
+                }
+                // assume the object was deleted
+                navigate('/customers')
+            }).catch((e) => {
+                console.log(e)
+            })
+        }}>Delete</button>
+        <br />
         <Link to={'/customers'}>Go back</Link>
         </>
     )
