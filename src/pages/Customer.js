45c76b32c6aa9582fa5cd4b4a8fa80cf0530dffd
danici <danici@email.com>
2023-11-14 10:03:03 +0100

    edit button

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -7,8 +7,16 @@ export default function Customer(){
     const { id } = useParams()
     const navigate = useNavigate()
     const [customer, setCustomer] = useState()
+    const [tempCustomer, setTempCustomer] = useState()
+    const [changed, setChanged] = useState(false)
     const [notFound, setNotFound] = useState()
     const url = baseUrl+'api/customers/'+id;
+
+    useEffect(() => {
+        // console.log('customer', customer)
+        // console.log('tempCustomer', tempCustomer)
+        // console.log(changed)
+    })
     useEffect(() => {
         fetch(url)
         .then((response) => {
@@ -23,20 +31,49 @@ export default function Customer(){
         }
     })
         .then((data) => {setCustomer(data.customer)
+                        setTempCustomer(data.customer)
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     
-    // function deleteCustomer() {
-    //     console.log('deleting...')
-    // }
+    function updateCustomer() {
+        const url = baseUrl + 'api/customers/' + id
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(tempCustomer)
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            setChanged(false)
+            setCustomer(data.customer)
+            console.log(data)
+        }).catch()
+    }
     return( 
         <>
         {notFound ? <p>The page of {id} is not in our database</p> : null}
         {customer ? <div>
-            <p>{customer.id}</p>
-            <p>{customer.name}</p>
-            <p>{customer.industry}</p>
+            {/* <p className="m-2 block px-2">{tempCustomer.id}</p> */}
+            <input className="m-2 block px-2" type="text" value={tempCustomer.name} onChange={(e) =>{
+                setChanged(true)
+                setTempCustomer({...tempCustomer, name: e.target.value})
+            }} />
+            <input className="m-2 block px-2" type="text" value={tempCustomer.industry} onChange={(e) =>{
+                setChanged(true)
+                setTempCustomer({...tempCustomer, industry: e.target.value})
+            }} />
+            {changed ? 
+            <>
+            <button onClick={(e) => {
+                setTempCustomer({ ...customer })
+                setChanged(false)
+            }}>Cancel</button> 
+            <button onClick={updateCustomer}>Save</button>
+            </>
+             : null}
         </div>: null}
         <button onClick={(e) => {
             const url = baseUrl+'api/customers/'+id
